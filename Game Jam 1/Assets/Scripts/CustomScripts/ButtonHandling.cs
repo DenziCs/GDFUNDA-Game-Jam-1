0aa65e637ab62b761140e860b8ac26a0df7d38c0
DenziCs <nathaniel_francis_filoteo@dlsu.edu.ph>
2021-08-04 16:36:04 +0800

    Added drawers; edited the button handling script to actually open and close the drawers.

--- Game Jam 1/Assets/Scripts/CustomScripts/ButtonHandling.cs
+++ Game Jam 1/Assets/Scripts/CustomScripts/ButtonHandling.cs
@@ -4,13 +4,70 @@ using UnityEngine;
 
 public class ButtonHandling : MonoBehaviour
 {
+    [SerializeField] private GameObject[] drawerButtons;
+
+    private const float OPEN_POS_Y = 1.0f;
+    private const float CLOSED_POS_Y = 9.0f;
+    private const float SPEED = 25.0f;
+
+    private GameObject activeDrawer = null;
+    private bool isOpening = false;
+    private bool isClosing = false;
+
     public void OpenDrawer(GameObject drawer)
     {
-        drawer.SetActive(true);
+        for (int i = 0; i < drawerButtons.Length; i++)
+        {
+            this.drawerButtons[i].SetActive(false);
+        }
+
+        this.activeDrawer = drawer;
+        this.isOpening = true;
     }
 
     public void CloseDrawer(GameObject drawer)
     {
-        drawer.SetActive(false);
+        for(int i = 0; i < drawerButtons.Length; i++)
+        {
+            this.drawerButtons[i].SetActive(true);
+        }
+
+        this.activeDrawer = drawer;
+        this.isClosing = true;
+    }
+
+    private void Update()
+    {
+        if (this.isOpening)
+        {
+            Vector2 currentPos = this.activeDrawer.transform.position;
+            currentPos.y -= SPEED * Time.deltaTime;
+            if(currentPos.y <= OPEN_POS_Y)
+            {
+                currentPos.y = OPEN_POS_Y;
+                this.isOpening = false;
+            }
+            this.activeDrawer.transform.position = currentPos;
+            if (currentPos.y <= OPEN_POS_Y)
+            {
+                this.activeDrawer = null;
+            }
+        }
+
+        if (this.isClosing)
+        {
+            Vector2 currentPos = this.activeDrawer.transform.position;
+            currentPos.y += SPEED * Time.deltaTime;
+            if (currentPos.y >= CLOSED_POS_Y)
+            {
+                currentPos.y = CLOSED_POS_Y;
+                this.isClosing = false;
+            }
+            this.activeDrawer.transform.position = currentPos;
+            if (currentPos.y >= CLOSED_POS_Y)
+            {
+                this.activeDrawer = null;
+            }
+        }
     }
 }
