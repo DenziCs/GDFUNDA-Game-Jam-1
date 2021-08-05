ae92a3769c27f3ba055b426177874f0ef098ade0
DenziCs <nathaniel_francis_filoteo@dlsu.edu.ph>
2021-08-06 00:03:46 +0800

    Fixed the spawning script to use the reference items themselves.

--- Game Jam 1/Assets/Scripts/CustomScripts/ObjectSpawning.cs
+++ Game Jam 1/Assets/Scripts/CustomScripts/ObjectSpawning.cs
@@ -19,31 +19,38 @@ public class ObjectSpawning : MonoBehaviour
         else return false;
     }
 
-    private GameObject Spawn(GameObject templateObject, Transform parentTransform)
-    {
-        GameObject obj = GameObject.Instantiate(templateObject, parentTransform);
-        obj.SetActive(true);
-        return obj;
-    }
-
     private void SpawnItemInDrawer()
     {
+        int itemIndex = Random.Range(0, itemReferenceList.Length);
+        while (itemReferenceList[itemIndex].activeSelf)
+        {
+            itemIndex = (itemIndex + 1) % (this.itemReferenceList.Length);
+        }
+        itemReferenceList[itemIndex].SetActive(true);
+
         int drawerIndex = Random.Range(0, drawerList.Length);
         while (!this.HasSpace(drawerIndex))
         {
             drawerIndex = (drawerIndex + 1) % (this.drawerList.Length);
         }
 
-        GameObject newItem = this.Spawn(itemReferenceList[Random.Range(0, itemReferenceList.Length)], drawerList[drawerIndex].transform);
         Vector2 spawnPosition = drawerList[drawerIndex].transform.position;
-        newItem.transform.position = spawnPosition;
-        this.itemList.Add(newItem);
+        spawnPosition.x += Random.Range(-0.1f, 0.1f);
+        spawnPosition.y += 0.3f; 
+
+        this.itemReferenceList[itemIndex].transform.position = spawnPosition;
+        this.itemList.Add(this.itemReferenceList[itemIndex]);
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        for(int i = 0; i < 12; i++)
+        for(int i = 0; i < this.itemReferenceList.Length; i++)
+        {
+            this.itemReferenceList[i].SetActive(false);
+        }
+
+        for(int i = 0; i < 1; i++)
         {
             this.SpawnItemInDrawer();
         }
