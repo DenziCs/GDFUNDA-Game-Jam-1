d7b9219d31860da5d2a9efae6491d924d2604563
PuffyThePuff <79883134+PuffyThePuff@users.noreply.github.com>
2021-08-05 22:28:55 +0800

    Added strikes to UI

--- Game Jam 1/Assets/Scripts/Broadcasting/EventNames.cs
+++ Game Jam 1/Assets/Scripts/Broadcasting/EventNames.cs
@@ -50,6 +50,11 @@ public class EventNames {
 		public const string ON_FIRST_SCAN = "FIRST_TARGET_SCAN";
 		public const string ON_FINAL_SCAN = "ON_FINAL_SCAN";
 	}
+
+	public class MP_Events
+    {
+		public const string ON_LOSE_GAME = "ON_LOSE_GAME";
+    }
 }
 
 
--- Game Jam 1/Assets/Scripts/CustomScripts/HUDDisplay.cs
+++ Game Jam 1/Assets/Scripts/CustomScripts/HUDDisplay.cs
@@ -6,12 +6,15 @@ using UnityEngine.UI;
 public class HUDDisplay : MonoBehaviour
 {
     [SerializeField] private Text scoreText;
+    [SerializeField] private Text lifeText;
+    private int score = 0;
+    private int life = 3;
 
     // Start is called before the first frame update
     void Start()
     {
-        EventBroadcaster.Instance.AddObserver(EventNames.ON_CORRECT_MATCH, addScore);
-        EventBroadcaster.Instance.AddObserver(EventNames.ON_WRONG_MATCH, subtractScore);
+        EventBroadcaster.Instance.AddObserver(EventNames.ON_CORRECT_MATCH, AddScore);
+        EventBroadcaster.Instance.AddObserver(EventNames.ON_WRONG_MATCH, LoseLife);
     }
 
     // Update is called once per frame
@@ -20,18 +23,16 @@ public class HUDDisplay : MonoBehaviour
         
     }
 
-    public void addScore()
+    public void AddScore()
     {
-        this.scoreText.text = "SCORE: TESTADD";
+        this.score += 100;
+        this.scoreText.text = "SCORE: " + score;
     }
 
-    public void subtractScore()
+    public void LoseLife()
     {
-        this.scoreText.text = "SCORE: TESTSUBTRACT";
-    }
-
-    public void OnGiveUp()
-    {
-        
+        this.life--;
+        this.lifeText.text = "STRIKES LEFT: " + life;
+        EventBroadcaster.Instance.PostEvent(EventNames.MP_Events.ON_LOSE_GAME);
     }
 }
