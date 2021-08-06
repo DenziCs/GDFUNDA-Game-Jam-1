2824fcdfc4dcd1eda3b556cc6db2a7d823d951c7
DenziCs <nathaniel_francis_filoteo@dlsu.edu.ph>
2021-08-06 22:41:32 +0800

    Please don't break everything...

--- Game Jam 1/Assets/Scripts/CustomScripts/MouseDrag.cs
+++ Game Jam 1/Assets/Scripts/CustomScripts/MouseDrag.cs
@@ -15,7 +15,7 @@ public class MouseDrag : MonoBehaviour
         {
             Vector3 mousePos;
             mousePos = Input.mousePosition;
-            mousePos = Camera.main.ScreenToViewportPoint(mousePos);
+            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
             this.gameObject.transform.localPosition = new Vector3(mousePos.x - startPosX, mousePos.y - startPosY, 40);
         }
     }
@@ -26,7 +26,7 @@ public class MouseDrag : MonoBehaviour
         {
             Vector3 mousePos;
             mousePos = Input.mousePosition;
-            mousePos = Camera.main.ScreenToViewportPoint(mousePos);
+            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
 
             startPosX = mousePos.x - this.transform.localPosition.x;
             startPosY = mousePos.y - this.transform.localPosition.y;
