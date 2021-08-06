d1c81a51f3a4a0e9209071e10cd0727fdb4beae1
PuffyThePuff <79883134+PuffyThePuff@users.noreply.github.com>
2021-08-06 21:49:38 +0800

    Added mouse dragging, made objects bouncier

---
+++ Game Jam 1/Assets/Scripts/CustomScripts/MouseDrag.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseDrag : MonoBehaviour
+{
+    private float startPosX;
+    private float startPosY;
+    private bool isBeingHeld = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isBeingHeld)
+        {
+            Vector3 mousePos;
+            mousePos = Input.mousePosition;
+            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+            this.gameObject.transform.localPosition = new Vector3(mousePos.x - startPosX, mousePos.y - startPosY, 40);
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePos;
+            mousePos = Input.mousePosition;
+            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+
+            startPosX = mousePos.x - this.transform.localPosition.x;
+            startPosY = mousePos.y - this.transform.localPosition.y;
+
+            isBeingHeld = true;
+        }
+
+    }
+
+    private void OnMouseUp()
+    {
+        isBeingHeld = false;
+    }
+}
