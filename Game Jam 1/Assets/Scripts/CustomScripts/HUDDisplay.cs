2b792a3bfb8f825b245cd8de1328d827a21009b5
PuffyThePuff <79883134+PuffyThePuff@users.noreply.github.com>
2021-08-06 22:40:14 +0800

    Update HUDDisplay.cs

--- Game Jam 1/Assets/Scripts/CustomScripts/HUDDisplay.cs
+++ Game Jam 1/Assets/Scripts/CustomScripts/HUDDisplay.cs
@@ -26,13 +26,13 @@ public class HUDDisplay : MonoBehaviour
     public void AddScore()
     {
         this.score += 100;
-        this.scoreText.text = "SCORE: " + score;
+        this.scoreText.text = "" + score;
     }
 
     public void LoseLife()
     {
         this.life--;
-        this.lifeText.text = "STRIKES LEFT: " + life;
+        this.lifeText.text = "" + life;
         EventBroadcaster.Instance.PostEvent(EventNames.MP_Events.ON_LOSE_GAME);
     }
 }
