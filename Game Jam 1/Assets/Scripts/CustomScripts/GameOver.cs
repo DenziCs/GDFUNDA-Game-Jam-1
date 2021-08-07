924f1c6a9a07bc1ef45926c2c7d767024d21bc29
DenziCs <nathaniel_francis_filoteo@dlsu.edu.ph>
2021-08-07 21:26:58 +0800

    Updated the GameOver script to animate the pink slip.

--- Game Jam 1/Assets/Scripts/CustomScripts/GameOver.cs
+++ Game Jam 1/Assets/Scripts/CustomScripts/GameOver.cs
@@ -8,6 +8,8 @@ public class GameOver : MonoBehaviour
 {
     [SerializeField] private GameObject pinkSlip;
     [SerializeField] private Text finalScoreDisplay;
+    private bool isOver = false;
+    private bool isPrinting = false;
 
     // Start is called before the first frame update
     void Start()
@@ -18,13 +20,25 @@ public class GameOver : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (this.isPrinting)
+        {
+            Vector2 currentPos = this.pinkSlip.transform.localPosition;
+            currentPos.y -= 100.0f * Time.deltaTime;
+            if (currentPos.y <= 0.0f)
+            {
+                currentPos.y = 0.0f;
+                this.isPrinting = false;
+            }
+            this.pinkSlip.transform.localPosition = currentPos;
+        }
     }
 
     void LoseGame()
     {
+        this.isOver = true;
         this.pinkSlip.SetActive(true);
         // this.finalScoreDisplay.text = this.currentScore.ToString();
+        this.isPrinting = true;
         SceneManager.LoadScene("Main Menu");
     }
 }
