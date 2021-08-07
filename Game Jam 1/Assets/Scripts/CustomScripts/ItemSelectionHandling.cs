ffa2a1ee9f795e649ed8172bfcb328eb437bc3d9
DenziCs <nathaniel_francis_filoteo@dlsu.edu.ph>
2021-08-07 17:54:27 +0800

    Added some of the game logic.

---
+++ Game Jam 1/Assets/Scripts/CustomScripts/ItemSelectionHandling.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemSelectionHandling : MonoBehaviour
+{
+    [SerializeField] private Text selectionLabel;
+
+    public GameObject selectedItem = null;
+
+    public void SelectItem(GameObject item)
+    {
+        this.selectedItem = item;
+        this.selectionLabel.text = item.name;
+    }
+
+    public void DeselectItem()
+    {
+        this.selectedItem = null;
+        this.selectionLabel.text = "---";
+    }
+}
